1289e32da4f44b550cf4f1c77ae2a54a75e5d328
qichang.dong <qichang.dong@videt.cn>
2024-04-09 18:01:30 +0800

    u

--- run.sh
+++ run.sh
@@ -1,10 +1,28 @@
 gpu=$1
 if [ -z $gpu ]; then
-    gpu=1
+    gpu=0
 fi
 export CUDA_VISIBLE_DEVICES=$gpu
 export DGLBACKEND=pytorch
 # 
 file=scripts/run_inference.py
-python $file 'contigmap.contigs=[150-150]' inference.output_prefix=test_outputs/test inference.num_designs=10 \
+
+# monomer test
+# python $file 'contigmap.contigs=[150-150]' \
+#     inference.output_prefix=test_outputs/binder_test \
+#     inference.num_designs=10 \
+#     inference.model_directory_path=/mnt/nas1/models/RFdiffusion \
+#     inference.input_pdb=examples/input_pdbs/insulin_target.pdb \
+#     2>&1  </dev/null | tee $file.log
+
+# binder test
+python $file \
+    inference.input_pdb=examples/input_pdbs/insulin_target.pdb \
+    'contigmap.contigs=[A1-150/0 10-20]' \
+    'ppi.hotspot_res=[A59,A83,A91]' \
+    denoiser.noise_scale_ca=0 \
+    denoiser.noise_scale_frame=0 \
+    inference.output_prefix=test_outputs/binder_insulin_target/binder_insulin_target \
+    inference.num_designs=10 \
+    inference.model_directory_path=/mnt/nas1/models/RFdiffusion \
     2>&1  </dev/null | tee $file.log
